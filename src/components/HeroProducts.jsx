4d57046824bb378f6b569d8047b9b26d90422eca
Ayman AlFarra <aymanelfarra2004@gmail.com>
2025-10-10 13:43:33 +0300

    update on hero products slider buttons

--- src/components/HeroProducts.jsx
+++ src/components/HeroProducts.jsx
@@ -1,19 +1,10 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import '../styles/heroProducts.css';
 import appleIcon from '../assets/images/apple-icon.svg';
 import heroProduct from '../assets/images/hero-product.svg';
 import arrowIcon from '../assets/images/arrow2-icon.svg';
 
 export default function HeroProducts() {
-  useEffect(() => {
-    const sliderBtns = document.querySelectorAll('.sliderBtn-container > div');
-    sliderBtns.forEach((el, index) => {
-      el.addEventListener('click', () => {
-        sliderBtns.forEach((el) => el.classList.remove('active'));
-        sliderBtns[index].classList.add('active');
-      });
-    });
-  }, []);
   return (
     <section className="hero-product">
       <HeroProductInfo />
@@ -39,13 +30,32 @@ function HeroProductInfo() {
 }
 
 function SliderBtn() {
+  const [activeBtn, setActiveBtn] = useState('btn3');
+  const activeBtnHandler = (btn) => {
+    setActiveBtn(btn);
+  };
   return (
     <div className="sliderBtn-container">
-      <div></div>
-      <div></div>
-      <div className="active"></div>
-      <div></div>
-      <div></div>
+      <div
+        onClick={() => activeBtnHandler('btn1')}
+        className={`${activeBtn === 'btn1' ? 'active' : ''}`}
+      ></div>
+      <div
+        onClick={() => activeBtnHandler('btn2')}
+        className={`${activeBtn === 'btn2' ? 'active' : ''}`}
+      ></div>
+      <div
+        onClick={() => activeBtnHandler('btn3')}
+        className={`${activeBtn === 'btn3' ? 'active' : ''}`}
+      ></div>
+      <div
+        onClick={() => activeBtnHandler('btn4')}
+        className={`${activeBtn === 'btn4' ? 'active' : ''}`}
+      ></div>
+      <div
+        onClick={() => activeBtnHandler('btn5')}
+        className={`${activeBtn === 'btn5' ? 'active' : ''}`}
+      ></div>
     </div>
   );
 }
